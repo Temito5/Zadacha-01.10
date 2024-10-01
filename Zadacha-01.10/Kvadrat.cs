3c696831dd37cd1b2511cf3e8afd789cb77e5a72
Staiko <Staiko@DESKTOP-NUVQ8TO>
2024-10-01 20:43:24 +0300

    Add project files.

---
+++ Zadacha-01.10/Kvadrat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha_01._10
+{
+    public class Kvadrat
+    {
+        private double a;
+        private double p;
+        private double s;
+        public double A
+        {
+            get { return a; }
+            set
+            { a = value; }
+        }
+
+        public Kvadrat(double a)
+        {
+            this.a = a;
+        }
+
+        public void PandS()
+        {
+               p = 4 * a;
+               s = a * a;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"P={p} S={s}");
+        }
+    }
+}
---
+++ Zadacha-01.10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha_01._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kvadrat a1 = new Kvadrat(double.Parse(Console.ReadLine()));
+            a1.PandS();
+            a1.Print();
+        }
+    }
+}
